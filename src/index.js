a50b3ae33cb27fd3c793e3933269159dc0a1da23
中村憲太郎 <k_nakamura@g-wise.co.jp>
2022-06-21 11:53:34 +0900

    引き分け表示を追加

--- src/index.js
+++ src/index.js
@@ -156,7 +156,11 @@ class Game extends React.Component {
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      if (this.state.stepNumber < 9) {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      } else {
+        status = '*** Draw ***';
+      }
     }
     // console.log(history);
     // console.log(moves);
